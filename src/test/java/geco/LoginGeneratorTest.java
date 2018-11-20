a6849cd54ea1b6eaf126c84efaa0120074eb58ca
Stix31 <twashgm@gmail.com>
2018-11-20 21:00:57 +0100

    #2 login-generator-2 Couvrir LoginGenerator à 100% par des tests unitaires

--- src/test/java/geco/LoginGeneratorTest.java
+++ src/test/java/geco/LoginGeneratorTest.java
@@ -1,5 +1,6 @@
 package geco;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -7,10 +8,18 @@ import static org.junit.Assert.*;
 
 public class LoginGeneratorTest {
 
+    private LoginService loginService;
+    private LoginGenerator loginGenerator;
     private String prenom;
     private String nom;
     private String prenom2;
     private String nom2;
+    private String nom3;
+    public String PaulDupongLog = "PDUP";
+    public String PierreDupardLog = "PDUP1";
+    public String JacquesDurantLog = "JDUR";
+    private String[] liste = {"JROL",
+            "BPER", "CGUR", "JDU", "JRAL", "JRAL1"};
 
     @Before
     public void setup() {
@@ -18,12 +27,26 @@ public class LoginGeneratorTest {
         nom = "Dupond";
         prenom2 = "Pierre";
         nom2 = "Dupard";
+        nom3 = "Durant";
+        loginService = new LoginService(liste);
+        loginGenerator = new LoginGenerator(loginService);
     }
 
     @Test
     public void generateLoginForNomAndPrenom() {
         // given
+        String nomPaul;
+        String prenomPaul;
+        String retour;
+        String retour2;
         // when
+        nomPaul = nom3;
+        prenomPaul = prenom;
+        retour = loginGenerator.generateLoginForNomAndPrenom(nomPaul,prenomPaul);
+        retour2 = loginGenerator.generateLoginForNomAndPrenom(nomPaul,prenomPaul);
+
         // then
+        Assert.assertEquals("PDUR", retour);
+        Assert.assertEquals("PDUR1", retour2);
     }
 }
