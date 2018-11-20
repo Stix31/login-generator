c0ffd7f7c32df901de9dca393d8d29a696f6809a
Stix31 <twashgm@gmail.com>
2018-11-20 17:36:47 +0100

    close #1 Couverture 100%

---
+++ src/test/java/geco/LoginGeneratorTest.java
@@ -0,0 +1,29 @@
+package geco;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LoginGeneratorTest {
+
+    private String prenom;
+    private String nom;
+    private String prenom2;
+    private String nom2;
+
+    @Before
+    public void setup() {
+        prenom = "Paul";
+        nom = "Dupond";
+        prenom2 = "Pierre";
+        nom2 = "Dupard";
+    }
+
+    @Test
+    public void generateLoginForNomAndPrenom() {
+        // given
+        // when
+        // then
+    }
+}
