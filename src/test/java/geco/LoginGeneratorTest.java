7a44ff4c19d4bf0ecfe452b6938b03629d312ae9
Stix31 <twashgm@gmail.com>
2019-11-19 14:19:36 +0100

    close #10 100% test done

--- src/test/java/geco/LoginGeneratorTest.java
+++ src/test/java/geco/LoginGeneratorTest.java
@@ -19,9 +19,18 @@ public class LoginGeneratorTest {
     // When
     loginGenerator = new LoginGenerator(loginService);
     // Given
-    String nom = "Ralling";
-    String prenom = "John";
+    String nom = "Dupond";
+    String prenom = "Paul";
+    String nom1 = "Ralling";
+    String prenom1 = "John";
+    String nom2 = "Rolling";
+    String prenom2 = "Jean";
+    String nom3 = "Dùrand";
+    String prenom3 = "Paul";
     // Then
-    Assert.assertEquals("JRAL2", loginGenerator.generateLoginForNomAndPrenom(nom, prenom));
+    Assert.assertEquals("PDUP", loginGenerator.generateLoginForNomAndPrenom(nom, prenom));
+    Assert.assertEquals("JRAL2", loginGenerator.generateLoginForNomAndPrenom(nom1, prenom1));
+    Assert.assertEquals("JROL1", loginGenerator.generateLoginForNomAndPrenom(nom2, prenom2));
+    Assert.assertEquals("PDUR", loginGenerator.generateLoginForNomAndPrenom(nom3, prenom3));
   }
 }
