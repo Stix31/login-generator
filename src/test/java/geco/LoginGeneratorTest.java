38ea2b832e6c3d707eef2b4c171802a020c53136
Stix31 <twashgm@gmail.com>
2019-11-19 14:19:36 +0100

    close #10 100% test done
    fix warning

--- src/test/java/geco/LoginGeneratorTest.java
+++ src/test/java/geco/LoginGeneratorTest.java
@@ -4,11 +4,8 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class LoginGeneratorTest {
-  LoginGenerator loginGenerator;
-  LoginService loginService;
+  private LoginService loginService;
   @Before
   public void setup () {
     loginService = new LoginService(new String[] {"JROL",
@@ -17,11 +14,20 @@ public class LoginGeneratorTest {
   @Test
   public void generateLoginForNomAndPrenom() {
     // When
-    loginGenerator = new LoginGenerator(loginService);
+    LoginGenerator loginGenerator = new LoginGenerator(loginService);
     // Given
-    String nom = "Ralling";
-    String prenom = "John";
+    String nom = "Dupond";
+    String prenom = "Paul";
+    String nom1 = "Ralling";
+    String prenom1 = "John";
+    String nom2 = "Rolling";
+    String prenom2 = "Jean";
+    String nom3 = "Dùrand";
+    String prenom3 = "Paul";
     // Then
-    Assert.assertEquals("JRAL2", loginGenerator.generateLoginForNomAndPrenom(nom, prenom));
+    Assert.assertEquals("PDUP", loginGenerator.generateLoginForNomAndPrenom(nom, prenom));
+    Assert.assertEquals("JRAL2", loginGenerator.generateLoginForNomAndPrenom(nom1, prenom1));
+    Assert.assertEquals("JROL1", loginGenerator.generateLoginForNomAndPrenom(nom2, prenom2));
+    Assert.assertEquals("PDUR", loginGenerator.generateLoginForNomAndPrenom(nom3, prenom3));
   }
 }
